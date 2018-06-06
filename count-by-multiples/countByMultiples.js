d28cbab06de14d98cb9e75e6c1d42f5e9d245fb8
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-06-06 04:34:29 -1000

    challenge #4 completed

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,8 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
+  var newArray = [];
+  for(var i = 1; i < length + 1; i++){
+    newArray.push(i * x);
+  }
+  return newArray;
 }
