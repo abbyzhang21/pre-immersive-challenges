b42223ae78f574d170e22e650223088c07f4630a
Abby Zhang <abbyzhang@abbys-mbp.home>
2018-06-07 12:29:23 -1000

    challenges #5 complete

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,13 @@
 module.exports = function descendingOrder(number) {
   // write code in here
+  if(typeof(number) == "number"){
+    var digits = ("" + number).split("");  //convert to array of strings
+    digits.sort();
+    digits.reverse();  //descending order
+    var dString = digits.join("");  //convert to string
+    var dNum = Number(dString);  //convert back to number
+    return dNum;
+  }else{
+    return "not a number!";
+  }
 }
