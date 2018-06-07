7cf161632cb9eb7ba1f9ec2d276350129a73b01d
Abby Zhang <abbyzhang@abbys-mbp.home>
2018-06-07 13:21:17 -1000

    challenges #3 complete

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,12 @@
 module.exports = function cookieProblem(array) {
   // write your code in here
+  var total = 0;
+  //select max number
+  var big = Math.max(...array);  
+  for(var i = 0; i < array.length; i++){
+    if(array[i] < big){
+      total += big - array[i];
+    }
+  }
+  return total;
 }
