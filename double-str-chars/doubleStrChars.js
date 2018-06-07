4e632b842c158f5e5cb8a3fc74ab2dbdda32d2de
Abby Zhang <abbyzhang@abbys-mbp.home>
2018-06-07 13:07:07 -1000

    challenges #6 complete

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,13 @@
 module.exports = function doubleStrChars(str) {
   // write your code in here
+  if(typeof(str) == "string"){
+    //split into char array
+    var newStr = str.split("").map(function(d){
+      return d + d;
+    //join updated array
+    }).join("")
+    return newStr;
+  }else{
+    return "not a string!";
+  }
 }
