c9a39b304e606ad7b3a1378cb4c8bd417b3a14bc
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-06-07 16:51:43 -1000

    challenges #7 complete

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,27 @@
 module.exports = function evenLadderPattern(num) {
   // write your code in here
-}
+  var arr = [];
+  var newArr;
+  var newA;
+    if(num <= 1){
+     return "";
+     }
+    if(num % 2 >= 0 && num !== 0){
+      for(var i = 2; i < num+1; i +=2){
+        //repeat i for ith times
+        var joinN = Array(i+1).join(i);
+        arr.push(joinN);
+        newArr = arr.join("\n");
+        
+    }}if(num % 2 !== 0  && num !== 1){
+      for(var i = 2; i < num; i +=2){
+        //repeat i for ith times
+        var joinN = Array(i).join(i);
+          
+        arr.push(joinN);
+        newA = arr.join("\n");
+  
+    }
+    }
+    return newArr;
+  }
