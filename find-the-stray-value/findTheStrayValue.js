71808ec99ed4f511ecb5cc2226fa5c925d891f88
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-06-12 02:42:58 -1000

    challenges #10 complete

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,15 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
+  var stray;
+  for(var i = 0; i < array.length; i++){
+    for(var j = i + 1; j < array.length; j++){
+      if(array[i] !== array[j]){
+       return stray = array[j];
+        
+      }
+    }
+  }
+  //return stray;
 }
+//(array[array.length - 1] !== array[i])
+//&& array[i] !== array[j+1]
