c11e714ab6671330a421e7e0c01f2ddd8283bd45
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-06-12 00:01:55 -1000

    challenges #9 complete

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,11 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+  var longest = '';
+  for(var i = 0; i < sen.length; i++){
+    //find word with the longest length 
+    if(sen[i].length > longest.length){
+      longest = sen[i];
+    }
+  }
+  return longest;
 }
