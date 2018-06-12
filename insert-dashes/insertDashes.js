e3ec57c83b1433f5099e40232a85d6adbdd3d114
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-06-12 04:27:52 -1000

    challenges #14 complete

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,15 @@
 module.exports = function insertDashes(num) {
   // write your code in here
+  var newN = '';
+  var d = ('' + num).split('');
+  //find two even number
+  for(var i = 0; i < d.length; i++){
+    if(d[i] % 2 === 0 && d[i+1] % 2 === 0){
+      //place dash between
+       newN += d[i] + '-';
+    }else{
+      newN += d[i];
+    }
+  }
+  return newN;
 }
